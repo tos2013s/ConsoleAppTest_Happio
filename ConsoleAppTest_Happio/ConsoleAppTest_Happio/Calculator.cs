d56b6474d2308a5da43bc9920ed4f027ea8f2968
Preeyapong <tos2013s@gmail.com>
2022-03-17 17:18:16 +0700

    Add files via upload

---
+++ ConsoleAppTest_Happio/ConsoleAppTest_Happio/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest_Happio
+{
+    class Calculator
+    {
+        public static void Q1_input4()
+        {
+            // Ask the user to type the first number.
+            Console.Write("Type a number, and then press Enter: ");
+            //double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            int a ;
+            int.TryParse(Console.ReadLine(), out a);
+            for (int i = 1; i <= a - 1; i++)
+            {
+                for (int j = 1; j <= (a - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Q2_input()
+        {
+            int thb;
+            Console.WriteLine("Q2: \r");
+            Console.Write("Input THB currency : ");
+            int.TryParse(Console.ReadLine(), out thb);
+            double won = 33.72;
+            double usd = 0.028;
+            double yen = 3.4;
+
+            double currency_won = (double)thb * won;
+            double currency_usd = (double)thb * usd;
+            double currency_yen = (double)thb * yen;
+            Console.WriteLine("------------------------\r");
+            Console.WriteLine("Output is : {0} WON",currency_won.ToString("0.00"));
+            Console.WriteLine("Output is : {0} USD", currency_usd.ToString("0.00"));
+            Console.WriteLine("Output is : {0} YES", currency_yen.ToString("0.00"));
+            Console.WriteLine("------------------------\n");
+
+        }
+        public static void Q3_input()
+        {
+            int gas_liter;
+            float km = 0.00001f;
+            Console.WriteLine("Q3: \r");
+            Console.Write("Inout Gas Liter :");
+            int.TryParse(Console.ReadLine(),out gas_liter);
+
+            int cal1 = gas_liter * 13000;
+            int cal2 = cal1 * 95 ; // cm
+            float cal3 = (float)cal2 * km; // แปลง cm เป็น km
+
+            Console.WriteLine("------------------------\r");
+            Console.WriteLine("Distance is : {0} KM", cal3.ToString("0.0"));
+            Console.WriteLine("------------------------\n");
+        }
+    }
+}
---
+++ ConsoleAppTest_Happio/ConsoleAppTest_Happio/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest_Happio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\r");
+            Console.WriteLine("\tHappio: Console Calculator in C#\n");
+            Console.WriteLine("------------------------------------------------------\n");
+            initialStart();
+            bool endApp = false;
+            // Display title as the C# console calculator app.
+
+            while (!endApp)
+            {
+                // Declare variables and set to empty.
+                // Ask the user to type the second number.
+                Console.Write("Your option? press Enter: ");
+                string op = Console.ReadLine();
+                // Declare variables and set to empty.
+                try
+                {
+                    if (op == "Q1")
+                    {                       
+                        Calculator.Q1_input4();
+                    }
+                    if (op == "Q2")
+                    {
+                        Calculator.Q2_input();
+                    }
+                    if (op == "Q3")
+                    {
+                        Calculator.Q3_input();
+                    }
+                    Console.WriteLine("\n");
+                    initialStart();
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+                // Ask the user to type the first number.
+                //Console.WriteLine("Q1 : input 4 in C#\r");
+                //Console.WriteLine("**********************\r");
+                //Console.Write("Type a number, and then press Enter: ");
+
+                //Q1_input4 = Console.ReadLine();
+                //Console.WriteLine("------------------------\n");
+
+                //Console.WriteLine("Q1 : input 5 r in C#\r");
+                //Console.WriteLine("**********************\r");
+                //Console.Write("Type a number, and then press Enter: ");
+                //Q1_input5 = Console.ReadLine();
+                //Console.WriteLine("------------------------\n");
+
+                //Console.WriteLine("**********************\r");
+                //Console.WriteLine("Q1 : input 6  in C#\r");
+                //Console.WriteLine("------------------------\r");
+                //Console.Write("Press Enter to continue... ");
+                //Q1_input6 = Console.ReadLine();
+                //Console.WriteLine("------------------------\n");
+
+                //Console.WriteLine("**********************\r");
+                //Console.WriteLine("Q3 : Enter  in C#\r");
+                //Console.WriteLine("------------------------\r");
+                //Console.Write("Press Enter to continue...");
+                //Q3_input = Console.ReadLine();
+                //Console.WriteLine("------------------------\n");
+
+                //Console.Write("Press Enter to exit...[y/n] : ");
+                //var line = "";
+                //line = Console.ReadLine();
+                //if (line == "y")
+                //{
+                //    Environment.Exit(0);
+                //}
+            }
+            return;
+        }
+        public static void initialStart()
+        {
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\r");
+            Console.WriteLine("\tEnter : Q1 = Input number 4,5,6");
+            Console.WriteLine("\tEnter : Q2 = Input THB currency");
+            Console.WriteLine("\tEnter : Q3 = Inout Gas Liter ");
+            Console.WriteLine("\n");
+        }
+    }
+}
